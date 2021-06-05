998fc13b84d90929a568fce3ff085c2e819ac860
Pavel Balashov <ba1ashpash@gmail.com>
2021-06-06 02:02:16 +0300

    Update flakes, fonts. Configure i3 bar.

--- home.nix
+++ home.nix
@@ -6,7 +6,7 @@
 			modifier = "Mod1";
 			terminal = "alacritty";
 			fonts = { 
-				names = ["DejaVu Sans Mono, FontAwesomeSFree"];
+				names = ["FontAwesomeSFree"];
 				style = "Bold Semi-Condensed";
 				size = 15.0;
 			};
@@ -22,12 +22,13 @@
 				"${modifier}+Shift+l" = "move left";
 			};
 			bars = [
-				{
-					fonts = {
-						 names = ["DejaVu Sans Mono, FontAwesomeSFree"];
-						 style = "Bold Semi-Condensed";
-						 size = 15.0;
+				{ 
+					fonts = { 
+						names = ["FontAwesomeSFree"];
+						style = "Bold Semi-Condensed";
+						size = 15.0;
 					};
+					statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-bottom.toml";
 				}
 			];
 
@@ -47,9 +48,68 @@
 		bash = {
 			enable = true;
 		};
-#		i3status-rust = {
-#			enable = true;
-#		};
+		i3status-rust = {
+			enable = true;
+			bars = {
+				bottom = {
+					blocks = [
+					{
+						block = "disk_space";
+						path = "/";
+						alias = "/";
+						info_type = "available";
+						unit = "GB";
+						interval = 60;
+						warning = 20.0;
+						alert = 10.0;
+					}
+					{
+						block = "net";
+						device = "wlp4s0";
+						format = "{ssid} {signal_strength} {ip} {speed_down;K*b} {graph_down;K*b}";
+						interval = 5;
+					}
+					{
+						block = "memory";
+						display_type = "memory";
+						format_mem = "{mem_used_percents}";
+						format_swap = "{swap_used_percents}";
+					}
+					{
+						block = "cpu";
+						interval = 1;
+					}
+					{
+						block = "load";
+						interval = 1;
+						format = "{1m}";
+					}
+					{ block = "sound"; }
+					{
+						block = "time";
+						interval = 60;
+						format = "%a %d/%m %R";
+					}
+					{
+						block = "battery";	
+						driver = "upower";
+						format = "{percentage} {time}";
+					}
+					];
+					settings = {
+						theme =  {
+							name = "solarized-dark";
+							overrides = {
+								idle_bg = "#123456";
+								idle_fg = "#abcdef";
+							};
+						};
+					};
+					icons = "awesome5";
+					theme = "gruvbox-dark";
+				};
+			};
+		};
 	};
 	home.username = "ba1ash";
 	home.homeDirectory = "/home/ba1ash";
--- hosts/x1c4.nix
+++ hosts/x1c4.nix
@@ -5,132 +5,141 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [
-      ./x1c4_hw.nix
-    ];
+	imports = [ ./x1c4_hw.nix ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+# Use the systemd-boot EFI boot loader.
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "x1c4";
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+	networking.hostName = "x1c4";
+	networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+	networking.wireless.networks = {
+		"HUAWEI-TKua" = {
+			pskRaw = "57f19f1f2e3d239651980af6009340d07e8387e31505147f022b6d8ffbbcb0ab";	
+		};
+	};
 
-  time.timeZone = "Europe/Minsk";
+	time.timeZone = "Europe/Minsk";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp0s31f6.useDHCP = true;
-  networking.interfaces.wlp4s0.useDHCP = true;
-  networking.interfaces.wwp0s20f0u2i12.useDHCP = true;
+# The global useDHCP flag is deprecated, therefore explicitly set to false here.
+# Per-interface useDHCP will be mandatory in the future, so this generated config
+# replicates the default behaviour.
+	networking.useDHCP = false;
+	networking.interfaces.enp0s31f6.useDHCP = true;
+	networking.interfaces.wlp4s0.useDHCP = true;
+	networking.interfaces.wwp0s20f0u2i12.useDHCP = true;
 
-  services.xserver = {
-    enable = true;
-    autorun = true;
-    layout = "us,ru";
-    xkbOptions = "caps:escape,ctrl:swap_lalt_lctl,altwin:swap_lalt_lwin,grp:rctrl_toggle";
-    desktopManager = {
-      xterm.enable = false;
-      xfce = {
-        enable = true;
-        noDesktop = true;
-        enableXfwm = false;
-      };
-    };
-    displayManager = {
-      defaultSession = "xfce+i3";
-      lightdm.greeters.mini ={
-        enable = true;
-        user = "ba1ash";
-        extraConfig = ''
-          [grreeter]
-          show-password-label = false
-          [greeter-theme]
-          background-image = ""
-        '';
-      };
-    };
-    windowManager.i3 = {
-      enable = true;
-      extraPackages = with pkgs; [
-        dmenu
-        i3status
-        i3lock
-        i3blocks
-      ];
-    };
-  };
+	services.xserver = {
+		enable = true;
+		autorun = true;
+		layout = "us,ru";
+		xkbOptions = "caps:escape,ctrl:swap_lalt_lctl,altwin:swap_lalt_lwin,grp:rctrl_toggle";
+		desktopManager = {
+			xterm.enable = false;
+			xfce = {
+				enable = true;
+				noDesktop = true;
+				enableXfwm = false;
+			};
+		};
+		displayManager = {
+			defaultSession = "xfce+i3";
+			lightdm.greeters.mini ={
+				enable = true;
+				user = "ba1ash";
+				extraConfig = ''
+					[grreeter]
+					show-password-label = false
+						[greeter-theme]
+						background-image = ""
+							'';
+			};
+		};
+		windowManager.i3 = {
+			enable = true;
+			extraPackages = with pkgs; [
+				dmenu
+					i3status
+					i3lock
+					i3blocks
+			];
+		};
+	};
 
-  console.useXkbConfig = true;
+	console.useXkbConfig = true;
 
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  # };
+# Select internationalisation properties.
+# i18n.defaultLocale = "en_US.UTF-8";
+# console = {
+#   font = "Lat2-Terminus16";
+#   keyMap = "us";
+# };
 
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+# Enable CUPS to print documents.
+# services.printing.enable = true;
 
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+# Enable sound.
+	sound.enable = true;
+	hardware.pulseaudio.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+# Enable touchpad support (enabled default in most desktopManager).
+# services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.ba1ash = {
-    isNormalUser = true;
-    home = "/home/ba1ash";
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  };
+# Define a user account. Don't forget to set a password with ‘passwd’.
+	users.users.ba1ash = {
+		isNormalUser = true;
+		home = "/home/ba1ash";
+		extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+	};
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim
-    alacritty
-    firefox
-  ];
+# List packages installed in system profile. To search, run:
+# $ nix search wget
+	environment.systemPackages = with pkgs; [
+		vim
+		alacritty
+		firefox
+		qutebrowser
+		zeal
+	];
 	environment.variables.EDITOR = "vim";
+	fonts.fonts = with pkgs; [
+		dejavu_fonts
+			font-awesome_5
+			powerline-fonts
+	];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-   nix = {
-	   package = pkgs.nixUnstable;
-	   extraOptions = ''
-		   experimental-features = nix-command flakes
-		   '';
-   };
+# Some programs need SUID wrappers, can be configured further or are
+# started in user sessions.
+# programs.mtr.enable = true;
+# programs.gnupg.agent = {
+#   enable = true;
+#   enableSSHSupport = true;
+# };
+	nix = {
+		package = pkgs.nixUnstable;
+		extraOptions = ''
+			experimental-features = nix-command flakes
+			'';
+	};
 
-  # List services that you want to enable:
+# List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+# Enable the OpenSSH daemon.
+	services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+# Open ports in the firewall.
+# networking.firewall.allowedTCPPorts = [ ... ];
+# networking.firewall.allowedUDPPorts = [ ... ];
+# Or disable the firewall altogether.
+# networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.09"; # Did you read the comment?
+# This value determines the NixOS release from which the default
+# settings for stateful data, like file locations and database versions
+# on your system were taken. It‘s perfectly fine and recommended to leave
+# this value at the release version of the first install of this system.
+# Before changing this value read the documentation for this option
+# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+	system.stateVersion = "20.09"; # Did you read the comment?
 
 }
