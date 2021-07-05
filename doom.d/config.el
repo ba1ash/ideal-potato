2cab8a00116c39dbdb459f7b6f2cedd2e359136f
Pavel Balashov <ba1ashpash@gmail.com>
2021-07-05 11:12:32 +0300

    Change projects search path for emacs

--- doom.d/config.el
+++ doom.d/config.el
@@ -31,7 +31,7 @@
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq! display-line-numbers-type 'relative)
-(setq! projectile-project-search-path '("~/projects"))
+(setq! projectile-project-search-path '("~/prj"))
 
 
 ;; Here are some additional functions/macros that could help you configure Doom:
